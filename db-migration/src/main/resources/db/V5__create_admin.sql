47bcc3f3d7cad2caeefb52a0dc3e946a07cbc464
CiobanuValentinEduard <valentin.ciobanu@avangarde-software.com>
2024-03-23 21:56:50 +0200

    fix admin scriot

--- db-migration/src/main/resources/db/V5__create_admin.sql
+++ db-migration/src/main/resources/db/V5__create_admin.sql
@@ -1,6 +1,6 @@
 -- Inserting the admin user
-INSERT INTO users (firstname, lastname, email, phone, active, created_date, modified_date, created_by, modified_by, password)
-VALUES ('admin', '', 'admin@ign.com', '', true, NOW(), NOW(), NULL, NULL, 'f7db06f2c5db810d1965af2eb22ef3a0b069f8c1af41728093047a47f7a53958:3999747e0b3676143612cccf211c8c33:2');
+INSERT INTO users (firstname, lastname, email, phone, active, validated, created_date, modified_date, created_by, modified_by, password)
+VALUES ('admin', '', 'admin@ing.com', '', true, true, NOW(), NOW(), NULL, NULL, 'f7db06f2c5db810d1965af2eb22ef3a0b069f8c1af41728093047a47f7a53958:3999747e0b3676143612cccf211c8c33:2');
 
 -- Inserting the administrator role
 INSERT INTO role (role_key, name)
