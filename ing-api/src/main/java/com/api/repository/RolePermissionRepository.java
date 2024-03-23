a8ad266f62ffa285d36221d44cf7d9f24702e02d
CiobanuValentinEduard <valentin.ciobanu@avangarde-software.com>
2024-03-23 20:08:31 +0200

    added role permission repository, fixed id from int to string for permission and role repo

--- ing-api/src/main/java/com/api/repository/RolePermissionRepository.java
+++ ing-api/src/main/java/com/api/repository/RolePermissionRepository.java
@@ -1,10 +1,12 @@
 package com.api.repository;
 
 import com.api.entities.Role;
+import com.api.entities.RolePermission;
+import com.api.entities.RolePermissionKey;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
-public interface RoleRepository extends JpaRepository<Role, String> {
+public interface RolePermissionRepository extends JpaRepository<RolePermission, RolePermissionKey> {
 }
